cb351f1d2f2f878a32b4b9e5dd5c60baf0517b34
corentinft <corentin.fouquet@outlook.com>
2019-03-18 14:07:52 +0100

    symphony

--- src/Controller/User.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Corentin
- * Date: 18/03/2019
- * Time: 11:17
- */
---
+++ src/Entity/Article.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $texte;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_cre;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_Maj;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(string $texte): self
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+
+    public function getDateCre(): ?\DateTimeInterface
+    {
+        return $this->date_cre;
+    }
+
+    public function setDateCre(\DateTimeInterface $date_cre): self
+    {
+        $this->date_cre = $date_cre;
+
+        return $this;
+    }
+
+    public function getDateMaj(): ?\DateTimeInterface
+    {
+        return $this->date_Maj;
+    }
+
+    public function setDateMaj(\DateTimeInterface $date_Maj): self
+    {
+        $this->date_Maj = $date_Maj;
+
+        return $this;
+    }
+}
